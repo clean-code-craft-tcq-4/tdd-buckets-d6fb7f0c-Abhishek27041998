e7d3ab10341ca03359ddab9c36a352bc50655a41
Kalburgi Abhishek Nagosa (SX/BSV-TC4) <AbhishekNagosa.Kalburgi@in.bosch.com>
2022-08-02 13:29:14 +0530

    TDD Step 3: Added more tests

--- test_detect_range_and_count_readings.py
+++ test_detect_range_and_count_readings.py
@@ -1,4 +1,4 @@
-from detect_range_and_count_readings import detect_range_and_count_readings
+from detect_range_and_count_readings import detect_range_and_count_readings, reset_values
 
 
 def test_2_inputs_numbers():
@@ -7,5 +7,26 @@ def test_2_inputs_numbers():
     outputs = detect_range_and_count_readings(inputs)
 
     assert outputs[0][(4, 5)] == 2
+
+
+def test_1_input_number():
+    inputs = [1]
+
+    outputs = detect_range_and_count_readings(inputs)
+
+    assert outputs[0][(1, 1)] == 1
+
+
+def test_reset_values():
+    inputs = [5, 3, 2]
+    index= 2
+
+    start, end , num = reset_values(inputs, index)
+
+    assert start == 2
+    assert end == 2
+    assert num == 1
+
+
 
 
